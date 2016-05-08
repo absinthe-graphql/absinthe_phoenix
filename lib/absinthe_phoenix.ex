c32c0f21a5384a771c160aaf50b0f9bbd44f1ee4
Ben Wilson <benwilson512@gmail.com>
2016-05-08 17:00:43 -0400

    failing test example'

--- lib/absinthe_phoenix.ex
+++ lib/absinthe_phoenix.ex
@@ -1,2 +1,16 @@
 defmodule Absinthe.Phoenix do
+  defmacro __using__(_) do
+    quote do
+      use Absinthe.Schema
+
+      plug Absinthe.Phoenix
+    end
+  end
+
+  def init(opts), do: opts
+
+  def call(conn, opts) do
+    IO.puts "yo"
+    conn |> IO.inspect
+  end
 end
--- test/support/controller.ex
+++ test/support/controller.ex
@@ -1,5 +1,10 @@
 defmodule Absinthe.Phoenix.TestController do
   use Phoenix.Controller
+  use Absinthe.Phoenix
+
+  query do
+    field :foo, :integer
+  end
 
   def index(conn, params) do
     conn
