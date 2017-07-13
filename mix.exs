d4278b541ca8fc0c95f11c635f82514b48ca10e8
Ben Wilson <benwilson512@gmail.com>
2017-07-12 22:57:21 -0400

    packaging stuff

--- mix.exs
+++ mix.exs
@@ -8,7 +8,20 @@ defmodule Absinthe.Phoenix.Mixfile do
      elixirc_paths: elixirc_paths(Mix.env),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     package: package(),
      deps: deps()]
+  end
+
+  defp package do
+    [description: "Subscription support via Phoenix for Absinthe, the GraphQL implementation for Elixir.",
+     files: ["lib", "mix.exs", "README*"],
+     maintainers: ["Ben Wilson", "Bruce Williams"],
+     licenses: ["BSD"],
+     links: %{
+       site: "http://absinthe-graphql.org",
+       github: "https://github.com/absinthe-graphql/absinthe_phoenix",
+      }
+    ]
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
@@ -23,6 +36,7 @@ defmodule Absinthe.Phoenix.Mixfile do
       {:absinthe, "~> 1.4.0-beta.1"},
       {:phoenix, "~> 1.0 or ~> 1.3.0-rc"},
       {:phoenix_pubsub, "~> 1.0 or ~> 1.3.0-rc"},
+      {:ex_doc, "~> 0.14", only: :dev},
       {:poison, "~> 2.0"},
     ]
   end
