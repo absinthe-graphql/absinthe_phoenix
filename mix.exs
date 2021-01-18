4759d4141fb6254f5e7726ad6a00d7618116fb9e
Thomas Citharel <github@tcit.fr>
2021-01-18 10:47:17 +0100

    Relax absinthe version requirement
    So that we can upgrade to 1.6

--- mix.exs
+++ mix.exs
@@ -42,8 +42,8 @@ defmodule Absinthe.Phoenix.Mixfile do
 
   defp deps do
     [
-      {:absinthe_plug, "~> 1.5.0"},
-      {:absinthe, "~> 1.5.0"},
+      {:absinthe_plug, "~> 1.5"},
+      {:absinthe, "~> 1.5"},
       {:decimal, "~> 1.0 or ~> 2.0"},
       {:phoenix, "~> 1.5"},
       {:phoenix_pubsub, "~> 2.0"},
