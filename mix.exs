8a8f44ca443467b897c7b059e2e8da474c9fd34b
Ben Wilson <benwilson512@gmail.com>
2024-04-02 13:04:04 -0400

    version bump

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Absinthe.Phoenix.Mixfile do
   use Mix.Project
 
   @source_url "https://github.com/absinthe-graphql/absinthe_phoenix"
-  @version "2.0.2"
+  @version "2.0.3"
 
   def project do
     [
