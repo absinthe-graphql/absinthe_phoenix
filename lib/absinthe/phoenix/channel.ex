b7b83814a92b7dc8a47e01fb903eff9255b6d03c
Bernard Duggan <bernard@circl.es>
2025-08-01 16:20:26 +1000

    Another attempt to fix crash on :ssl message

--- lib/absinthe/phoenix/channel.ex
+++ lib/absinthe/phoenix/channel.ex
@@ -88,6 +88,12 @@ defmodule Absinthe.Phoenix.Channel do
     {:reply, {:ok, %{subscriptionId: doc_id}}, socket}
   end
 
+  def handle_info({:ssl, _, _}, socket) do
+    # I have no idea where this message started coming from, but there's not much we can do
+    # with it that I know of.
+    {:noreply, socket}
+  end
+
   def handle_info(
         %Phoenix.Socket.Broadcast{
           payload: %{result: %{ordinal: ordinal, ordinal_compare_fun: ordinal_compare_fun}}
@@ -114,18 +120,10 @@ defmodule Absinthe.Phoenix.Channel do
   end
 
   defp send_msg(msg, socket) do
-    stripped_msg =
-      case msg do
-        %Phoenix.Socket.Broadcast{payload: %{result: _}} ->
-          {_ordinal, msg} = pop_in(msg.payload.result[:ordinal])
-          {_ordinal_compare_fun, msg} = pop_in(msg.payload.result[:ordinal_compare_fun])
-          msg
+    {_ordinal, msg} = pop_in(msg.payload.result[:ordinal])
+    {_ordinal_compare_fun, msg} = pop_in(msg.payload.result[:ordinal_compare_fun])
 
-        _ ->
-          msg
-      end
-
-    encoded_msg = socket.serializer.fastlane!(stripped_msg)
+    encoded_msg = socket.serializer.fastlane!(msg)
     send(socket.transport_pid, encoded_msg)
   end
 
